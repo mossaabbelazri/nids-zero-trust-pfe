af74f8215124f1c918e048dbbf6928597543521d
mossaabbelazri <belazrimossaab@gmail.com>
2026-08-25 22:07:54 +0100

    scale: increase GKE initial node count to 3 for monitoring capacity

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "google_container_cluster" "primary" {
   location = "europe-west1-b" # Utilise une ZONE précise plutôt qu'une REGION entière pour économiser les nœuds
 
 # Configuration du mode Standard ultra-léger
-  initial_node_count = 2 
+  initial_node_count = 3 
 
   node_config {
     machine_type = "e2-medium" # Machine standard et économique
